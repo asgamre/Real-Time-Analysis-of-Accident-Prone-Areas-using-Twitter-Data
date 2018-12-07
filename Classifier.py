b854dc6aebd67d8af739558315b07d7d087cadbb
Ameya Gamre <AG2608@lanvera.org>
2018-12-06 21:23:13 -0600

    Second Commit

--- Classifier.py
+++ Classifier.py
@@ -1,44 +1,37 @@
-import re, datetime
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.linear_model import SGDClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.externals import joblib
 
-Count_Vectorizer, Tfidf_Transformer, cachedStopwords, porter= None, None, None, None
+Count_Vectorizer, porter= None, None
 
-
-# defines the required global variables, as well as stopwords or stemmers required
-def initializeCV(i,j):
-    global Count_Vectorizer, cachedStopwords, porter
+# defines the required global variables
+def initialize(i,j):
+    global Count_Vectorizer, porter
     print("Ngram range {},{}".format(i,j))
     Count_Vectorizer = CountVectorizer(ngram_range=(i, j))  # change settings for unigram, bigram, trigram
-    cachedStopwords = stopwords.words("english")
     porter = PorterStemmer()
 
-# takes in a pandas dataframe and returns a feature vector and list of interests which act as labels
+# takes in a pandas dataframe and returns a feature vector and list of labels
 def getFeatureVectorAndLabels(dataframe):
-    global Tfidf_Transformer, selector
     print()
     print("Converting dataframe to list of tweets and list of interests")
-    listOfTweets, interestLabels = dfToTweetsAndInterests(dataframe)
+    listOfTweets, labels = dfToTweetsAndLabels(dataframe)
     print("Stemming list of tweets")
-    listOfTweets = stemList(porter, listOfTweets)  # change stemming algorithm here
+    listOfTweets = stemList(porter, listOfTweets)
     data_counts = Count_Vectorizer.fit_transform(listOfTweets)
     print("There are %s features" % data_counts.shape[1])
     print()
-    temp_tfidf_transformer = TfidfTransformer(use_idf=True).fit(data_counts)
-    Tfidf_Transformer = temp_tfidf_transformer
     tfidf_doc = TfidfTransformer(use_idf=True).fit_transform(data_counts)
-    return tfidf_doc, interestLabels
+    return tfidf_doc, labels
 
-
-# takes in a stemmer object defined in initalizeCV() and a list of strings to be stemmed
+# takes in a stemmer object defined in initalize() and a list of strings to be stemmed
 def stemList(stemmer, listOfTweets):
     if stemmer != None:
         stemmedTokens = []
@@ -51,9 +44,8 @@ def stemList(stemmer, listOfTweets):
             listOfTweets.append(" ".join(str(i) for i in token))
     return listOfTweets
 
-
-# Returns a cleaned dataset without punctuation and stopwords
-def dfToTweetsAndInterests(dataframe):
+# Returns a list of tweets and labels
+def dfToTweetsAndLabels(dataframe):
     listOfTweets = []
     interestLabels = []
     for i in dataframe.index:
@@ -63,22 +55,9 @@ def dfToTweetsAndInterests(dataframe):
         interestLabels.append(interest)
     return listOfTweets, interestLabels
 
-
-# train support vector machine classifier with given features and labels
-def trainSVM(features, labels, kernelType):
-    # starttime = datetime.datetime.now()
-    if kernelType == 'sgd':
-        clf = SGDClassifier().fit(features, labels)
-    # kernelType can be linear, poly, rbf or sigmoid
-    else:
-        clf = SVC(kernel=kernelType).fit(features, labels)
-    # print("Time taken to train SVMClassifier(" + kernelType + ") : " + str(datetime.datetime.now() - starttime))
-    return clf
-
-
 # returns an interest prediction
-def predictInterest(targetHandle, classifier):
-    data_counts = Count_Vectorizer.transform(targetHandle)
+def predictInterest(tweet,classifier,bestCount_Vectorizer):
+    data_counts = bestCount_Vectorizer.transform(tweet)
     tfidf_doc = TfidfTransformer(use_idf=True).fit(data_counts).transform(data_counts)
     prediction = classifier.predict(tfidf_doc)
     return prediction
@@ -86,9 +65,7 @@ def predictInterest(targetHandle, classifier):
 # uses the cross_val_score method to calculate the accuracy of a model using kfold cross validation, with cv being the number of folds
 def printKFoldScore(classifier, features, labels, name):
     kfold_score = cross_val_score(classifier, features, labels, cv=10)
-    print("Accuracy for " + name + ": " + str(kfold_score.mean()))
-    print()
-
+    return kfold_score.mean()
 
 # takes a prediction using the desired classifier and prints the classification report and confusion matrix
 def printMetrics(classifier, features, labels, name):
@@ -97,6 +74,7 @@ def printMetrics(classifier, features, labels, name):
     print("Classification report for " + name)
     print(classification_report(labels, predictedList))
     cm = confusion_matrix(labels, predictedList)
+    print("Confusion Matrix")
     print(cm)
     # cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     # np.set_printoptions(precision=2)
@@ -119,14 +97,15 @@ def plotMatrix(cm, title='Confusion matrix', cmap=plt.cm.YlOrRd):
 
 
 # runs a classifier against a manually curated dataset and calculates the accuracy
-def testClassifier(classifier, dataframe):
+def testClassifier(classifier, dataframe,bestCount_Vectorizer):
+    print("N-gram Range:".format(bestCount_Vectorizer.ngram_range))
     interestLabels, predictedInterests, listOfTweets = [], [], []
     print("Running on test set")
     for i in dataframe.index:
         tweet = dataframe["Tweet"][i]
         interest = dataframe["Label"][i]
         interestLabels.append(interest)
-        predictedInterests.append(predictInterest([tweet], classifier))
+        predictedInterests.append(predictInterest([tweet], classifier,bestCount_Vectorizer))
         listOfTweets.append(tweet)
     if len(interestLabels) == len(predictedInterests):
         correct = 0
@@ -134,32 +113,51 @@ def testClassifier(classifier, dataframe):
         for j in range(len(interestLabels)):
             if interestLabels[j] == predictedInterests[j]:
                 correct += 1
-            else:
-                print(listOfTweets[j] + " is " + interestLabels[j] + " but predicted as " + str(predictedInterests[j][0]))
-        print()
-        print(str(correct) + "/" + str(len(interestLabels)) + " tweets predicted correctly")
-        print()
+        print("{}% accuracy".format(correct/len(interestLabels)))
 
 
+# train multinomial naive bayes classifier with given features and labels
+def trainNB(features,labels):
+	starttime = datetime.datetime.now()
+	clf = MultinomialNB().fit(features, labels)
+	print("Time taken to train NBClassifier: " + str(datetime.datetime.now() - starttime))
+	return clf
+
+
+# takes in the training set and returns a trained object and the best ngram feature object
 def train(trainingSet):
+    print("Initializing variables and environment")
     ngrams = [(1,1),(2,2),(3,3),(1,2),(1,3)]
+    best_accuracyNB = None
+    best_NBClassifier = None
+    best_tfidf = None
+    bestCount_Vectorizer = None
     for (i,j) in ngrams:
-        initializeCV(i,j)
+        initialize(i,j)
         tfidf_doc, interestLabels = getFeatureVectorAndLabels(trainingSet)
         loadFromSave = False
         if loadFromSave:
             # joblib is an sklearn library that allows us to save / load the trained classifiers
-            SVMClassifier = joblib.load('classifiers/svm.pkl')
-            print("Classifier loaded from file")
+            NBClassifier = joblib.load('classifiers/naivebayes.pkl')
+            print("Classifiers loaded from file")
         else:
-            SVMClassifier = trainSVM(tfidf_doc, interestLabels, "sgd")
-            joblib.dump(SVMClassifier, 'classifiers/svm.pkl')
+            NBClassifier = trainNB(tfidf_doc, interestLabels)
+            joblib.dump(NBClassifier, 'classifiers/naivebayes.pkl')
 
-        # kfold score for SVMClassifier
-        printKFoldScore(SVMClassifier,tfidf_doc,interestLabels,"SVMClassifier")
+        accuracyNB = printKFoldScore(NBClassifier, tfidf_doc, interestLabels, "NBClassifier")
+        print("Accuracy for NBClassifier: " + str(accuracyNB))
+        printMetrics(NBClassifier, tfidf_doc, interestLabels, "NBClassifier")
 
-        # classification report and confusion matrix for SVMClassifier
-        printMetrics(SVMClassifier,tfidf_doc,interestLabels,"SVMClassifier")
+        if not best_accuracyNB:
+            best_accuracyNB = accuracyNB
+            best_NBClassifier = NBClassifier
+            bestCount_Vectorizer = Count_Vectorizer
+            best_tfidf = tfidf_doc
+        if accuracyNB > best_accuracyNB:
+            best_accuracyNB = accuracyNB
+            best_NBClassifier = NBClassifier
+            bestCount_Vectorizer = Count_Vectorizer
+            best_tfidf = tfidf_doc
 
-
-    return SVMClassifier
+    return best_NBClassifier,bestCount_Vectorizer,best_tfidf,interestLabels
+    # return NBClassifier,Count_Vectorizer
--- Connector.py
+++ Connector.py
@@ -2,30 +2,36 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 import pandas as pd
 import Credentials
-from Classifier import train,predictInterest,testClassifier
+from Preprocess import preprocess
+from Classifier import train,predictInterest,testClassifier,printMetrics
 targetWords = ['crash', 'accident', 'wreck','mishap']
-class listener(StreamListener):
+label = {'S':'Situational','N':'Non-Situational'}
 
+class listener(StreamListener):
     def __init__(self):
-        global SVMClassifier
+        global SVMClassifier, NBClassifier, bestCount_Vectorizer
         super().__init__()
         print("Initializing")
-        trainingSet = pd.read_csv("dataset/train.csv",usecols=['Tweet','Label'])
-        testSet = pd.read_csv("dataset/test.csv",usecols=['Tweet','Label'])
+        trainingSet = pd.read_csv("dataset/train.csv", usecols=['Tweet', 'Label'])
+        testSet = pd.read_csv("dataset/test.csv", usecols=['Tweet', 'Label'])
         print("Initiating Training...")
         print()
-        SVMClassifier = train(trainingSet)
+        NBClassifier, bestCount_Vectorizer, best_tfidf, interestLabels = train(trainingSet)
+        print()
         print("Initiating Testing...")
         print()
-        testClassifier(SVMClassifier, testSet)
+        testClassifier(NBClassifier, testSet, bestCount_Vectorizer)
+        printMetrics(NBClassifier, best_tfidf, interestLabels, "NBClassifier")
+        print()
         print("Starting Live tweets")
 
     def on_status(self, status):
         tweet = str(status.text).lower()
         if any(i in tweet for i in targetWords):
-            label = predictInterest([tweet], SVMClassifier)
+            preprocessedtweet = preprocess(tweet)
+            predictedlabel = predictInterest([preprocessedtweet], NBClassifier, bestCount_Vectorizer)
             print(tweet)
-            print(label[0])
+            print(label[predictedlabel[0]])
 
     def on_error(self, status_code):
         if status_code == 420:
